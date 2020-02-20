53a6430a031d044fa45733eef02e0ba746dc000a
Aaron 'Nox' Sandgren <aaron.sandgren@live.se>
2020-02-20 23:07:36 +0100

    auth is kinda working with returning a value to the relay

--- node/app.js
+++ node/app.js
@@ -1,8 +1,13 @@
 const express = require("express")
-
+var bodyParser = require('body-parser')
 const app = express();
 
 const port = 3040;
+
+app.use(bodyParser.json({
+    extended: false
+}))
+
 
 app.listen(port, () => console.log(`listening on ${port}`))
 
@@ -10,10 +15,21 @@ app.get('/', (req, res) => {
     res.send("danks memes")
 })
 
-app.get('/auth', (req, res) => {
-    console.log(req.query.token)
 
-    let data = {data: false}
+app.post('/auth', (req, res) => {
+    console.log(req.body.token)
 
-    res.send(data)
+    if (req.body.token == 'digit') {
+        let data = {
+            data: true
+        }
+        res.send(data)
+    }else{
+        let data = {
+            data: false
+        }
+        res.send(data)
+    }
+    
+
 })
